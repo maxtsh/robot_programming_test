15ee1f1da4d8c1b4a686fdd6212e478b880414e8
Mohammad Tat Shahdoost <tatshahdoost@gmail.com>
2023-04-23 23:10:22 +0330

    added input and form events and robot movement logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,76 @@
-import { Fragment, useState } from "react";
+import { ChangeEvent, FormEvent, Fragment, useState } from "react";
 
 const size = 6;
-
+const englishRegex = /^[^fFlLrR]+$/;
+const swedishRegex = /^[^vVhHgG]+$/;
+const frenchRegex = /^[^dDaAgG]+$/;
+const dirs = ["N", "E", "S", "W"];
+const lefts = ["l", "v", "g"];
+const rights = ["r", "h", "d"];
+const forwards = ["f", "g", "a"];
 const arrY = [...Array(size)].map(() => crypto.randomUUID());
 const arrX = [...Array(size)].map(() => crypto.randomUUID());
 
 const App: React.FC = () => {
   const [coord, setCoord] = useState({ x: 0, y: 0, dir: "N", command: "" });
+
+  const handleChange = ({
+    target: { name, value },
+  }: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setCoord((prev) => ({
+      ...prev,
+      [name]: !isNaN(Number(value)) ? Number(value) : value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { x, y, dir, command } = coord;
+
+    const cmd = command.toLowerCase();
+
+    let lang = "en";
+    if (cmd.includes("v") || cmd.includes("h")) lang = "se";
+    if (cmd.includes("a") || cmd.includes("d")) lang = "fr";
+
+    let tempX = x;
+    let tempY = y;
+    let tempDir = dir;
+    const lastDirsIdx = dirs.length - 1;
+
+    for (let idx = 0; idx <= cmd.length; idx++) {
+      const c = cmd.charAt(idx).toLowerCase();
+      const currDirIdx = dirs.findIndex((item) => item === tempDir);
+
+      if (rights.includes(c)) {
+        if (currDirIdx === lastDirsIdx) {
+          tempDir = dirs[0];
+        } else {
+          tempDir = dirs[currDirIdx + 1];
+        }
+      }
+      if (lefts.includes(c)) {
+        if (currDirIdx === 0) {
+          tempDir = dirs[lastDirsIdx];
+        } else {
+          tempDir = dirs[currDirIdx - 1];
+        }
+      }
+      if (forwards.includes(c)) {
+        if (tempDir === "N") tempY = tempY - 1;
+        if (tempDir === "S") tempY = tempY + 1;
+        if (tempDir === "E") tempX = tempX + 1;
+        if (tempDir === "W") tempX = tempX - 1;
+      }
+    }
+
+    setCoord((prev) => ({
+      ...prev,
+      dir: tempDir,
+      x: tempX,
+      y: tempY,
+    }));
+  };
 
   return (
     <main>
@@ -32,11 +96,11 @@ const App: React.FC = () => {
             ))}
           </div>
         </div>
-        <form className="wrapper__form">
+        <form className="wrapper__form" onSubmit={handleSubmit}>
           <div className="wrapper__form__row">
             <select
-              id="direction"
-              name="direction"
+              name="dir"
+              onChange={handleChange}
               className="wrapper__form__row__input"
             >
               <option value="N">North (N)</option>
@@ -50,6 +114,7 @@ const App: React.FC = () => {
               max="9"
               type="number"
               placeholder="Enter X"
+              onChange={handleChange}
               className="wrapper__form__row__input"
             />
             <input
@@ -58,6 +123,7 @@ const App: React.FC = () => {
               max="9"
               type="number"
               placeholder="Enter Y"
+              onChange={handleChange}
               className="wrapper__form__row__input"
             />
           </div>
@@ -67,6 +133,7 @@ const App: React.FC = () => {
               type="text"
               id="command"
               name="command"
+              onChange={handleChange}
               className="wrapper__form__row__input"
             />
             <input
